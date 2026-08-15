fa7cf3b491be9452518557e9d619ea6989f9e531
mohana-creator-33 <mohanashanmuganathan@gmail.com>
2026-08-15 10:56:10 +0530

    Add files via upload

---
+++ com.jsx
@@ -0,0 +1,13 @@
+// const Comu="HTML"
+function Course(){
+    return(
+        <div>
+            {/* <img src="doraemon.jpg" alt="doraemon"> */}
+            <h3>{Comu}</h3>
+            <p>haii.. welcome to all</p>
+            {/* </img> */}
+        </div>
+    );
+}
+
+export default Course
